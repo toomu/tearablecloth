47c9b681135d5baefecbc2f5c254426caf536ddf
Kamal Reddy <kamal.is.reddy@gmail.com>
2018-03-13 23:22:27 +0530

    vqe

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
-
 var app = express();
-
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -36,7 +32,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({'error':err});
 });
 
 module.exports = app;
